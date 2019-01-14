ae96709b3990deb535acf26b4f0d72ee072d6f66
jack00000 <1558185399@qq.com>
2019-01-14 22:27:57 +0800

    add 。sql

--- ssm (3)/src/main/java/com/whgc/service/CategoryService.java
+++ ssm (3)/src/main/java/com/whgc/service/CategoryService.java
@@ -6,6 +6,12 @@ import com.whgc.pojo.Category;
 import com.whgc.util.Page;
 
 public interface CategoryService {
-
-	List<Category> list();
+	int total();
+    List<Category> list(Page page);
+ 
+    void add(Category category);
+ 
+    void delete(int id);
+ 
+    Category get(int id);
 }
