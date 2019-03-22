c394e57e89097773753039f25b64ebc89f030e4d
fangjun <fangjun@esensoft.com>
2019-03-22 11:38:52 +0800

    solr 查询界面 有地址 微信小程序可以通过api访问 1.http://localhost:8983/solr/how2java/select?q=name:鸭绒 2.fl 参数是默认返回字段 score 是评分  solr内置的内容匹配度评分排序机制

--- ssm (3)/src/main/java/com/whgc/test/testSolr/SolrUtil.java
+++ ssm (3)/src/main/java/com/whgc/test/testSolr/SolrUtil.java
@@ -1,12 +1,17 @@
 package com.whgc.test.testSolr;
 import java.io.IOException;
 import java.util.List;
- 
+
 import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.beans.DocumentObjectBinder;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
 import org.apache.solr.common.SolrInputDocument;
+import org.apache.solr.common.util.NamedList;
  
 public class SolrUtil {
     public static SolrClient client;
@@ -28,6 +33,39 @@ public class SolrUtil {
         }
         client.commit();
         return true;
+    }
+    public static void queryHighlight(String keywords) throws SolrServerException, IOException {
+        SolrQuery q = new SolrQuery();
+        //开始页数
+        q.setStart(0);
+        //每页显示条数
+        q.setRows(10);
+        // 设置查询关键字
+        q.setQuery(keywords); 
+        // 开启高亮
+        q.setHighlight(true); 
+        // 高亮字段
+        q.addHighlightField("name"); 
+        // 高亮单词的前缀
+        q.setHighlightSimplePre("<span style='color:red'>"); 
+        // 高亮单词的后缀
+        q.setHighlightSimplePost("</span>"); 
+        //摘要最长100个字符
+        q.setHighlightFragsize(100);
+        //查询
+        QueryResponse query = client.query(q);
+        //获取高亮字段name相应结果
+        NamedList<Object> response = query.getResponse();
+        NamedList<?> highlighting = (NamedList<?>) response.get("highlighting");
+        for (int i = 0; i < highlighting.size(); i++) {
+            System.out.println(highlighting.getName(i) + "：" + highlighting.getVal(i));
+        }
+        
+        //获取查询结果
+        SolrDocumentList results = query.getResults();
+        for (SolrDocument result : results) {
+            System.out.println(result.toString());
+        }
     }
  
 }
--- ssm (3)/src/main/java/com/whgc/test/testSolr/TestSolr4j.java
+++ ssm (3)/src/main/java/com/whgc/test/testSolr/TestSolr4j.java
@@ -5,7 +5,9 @@ import org.apache.solr.client.solrj.SolrServerException;
  
 public class TestSolr4j {
     public static void main(String[] args) throws SolrServerException, IOException {
-        List<Product> products = ProductUtil.file2list("D:\\140k_products.txt");
-        SolrUtil.batchSaveOrUpdate(products);
+//        List<Product> products = ProductUtil.file2list("D:\\140k_products.txt");
+//        SolrUtil.batchSaveOrUpdate(products);
+    	//高亮查询查询
+        SolrUtil.queryHighlight("name:手机");
     }
 }
